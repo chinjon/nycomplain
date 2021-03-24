6b692cc2012a3477c01cc3178c0fd7857b50f3ba
Jonathan Chin <jonathankchin@gmail.com>
2021-03-24 19:47:26 -0400

    feat: move search query into variable

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const axios = require('axios')
 require('dotenv').config()
-const fs = require('fs')
+const axios = require('axios')
+
+const searchQuery = `$where=created_date between '2017-12-31T20:00:00' and '2017-12-31T23:59:59'`;
+
 axios({
   method: 'get',
-  url: `https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=created_date between '2017-12-31T20:00:00' and '2017-12-31T23:59:59'`,
+  url: `https://data.cityofnewyork.us/resource/erm2-nwe9.json?${searchQuery}`,
   data: {
     "$limit" : 5000,
     "$$app_token" : `${process.env.APP_TOKEN}`
