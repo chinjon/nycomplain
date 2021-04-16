e01d6f73d244b1b0a15284c2598ffbd3db4f636c
Jonathan Chin <jonathankchin@gmail.com>
2021-04-16 18:37:18 -0400

    chore: move file, create attribution tile

---
+++ src/components/attribution-tile/attribution-tile.js
@@ -0,0 +1,10 @@
+import { TileLayer } from 'react-leaflet';
+
+function AttributionTile() {
+  return (<TileLayer
+  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />)
+}
+
+export default AttributionTile;
--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,13 +1,11 @@
 import './map.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import AttributionTile from './../attribution-tile/attribution-tile';
+import { MapContainer, Marker, Popup } from 'react-leaflet';
 
 function Map() {
   return (
     <MapContainer center={[40.65, -73.91]} zoom={13} scrollWheelZoom={false} id="mapid">
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <AttributionTile />
       <Marker position={[40.65, -73.91]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
