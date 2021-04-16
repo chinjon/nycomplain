9764c8c2c5a4a9a7dfb31e05ee874dca577cce21
Jonathan Chin <jonathankchin@gmail.com>
2021-04-16 17:41:04 -0400

    feat: move out map component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Map from './components/map'
 
 function App() {
   return (
     <div className="App">
-      <MapContainer center={[40.65, -73.91]} zoom={13} scrollWheelZoom={false} id="mapid">
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[40.65, -73.91]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <div className="map-container">
+        <Map />
+      </div>
     </div>
   );
 }
