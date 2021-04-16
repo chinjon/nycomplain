d54518e5f95ac5fdb7eb5edb2b7d71ecbbe2ffba
Jonathan Chin <jonathankchin@gmail.com>
2021-04-16 17:34:12 -0400

    feat: init commit of map component

---
+++ src/components/map.js
@@ -0,0 +1,20 @@
+import './map.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+function Map() {
+  return (
+    <MapContainer center={[40.65, -73.91]} zoom={13} scrollWheelZoom={false} id="mapid">
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[40.65, -73.91]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default Map;
