de315d896a77f797aa24685011ec21534ba748ef
Jonathan Chin <jonathankchin@gmail.com>
2021-04-17 17:08:25 -0400

    test: add assertopm

--- src/api/test/index.spec.js
+++ src/api/test/index.spec.js
@@ -1,5 +1,9 @@
 import api from './../index';
 
-it('should be a promise', () => {
+it('should be a function', () => {
   expect(typeof api).toBe('function')
+});
+
+it('should return a promise', () => {
+  expect(typeof api()).toBe('object')
 });
