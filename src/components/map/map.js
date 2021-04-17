283e7ed3206c8717777654babca1150de48338b8
Jonathan Chin <jonathankchin@gmail.com>
2021-04-17 01:17:47 -0400

    feat: add map marker

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,16 +1,13 @@
 import './map.css';
 import AttributionTile from './../attribution-tile/attribution-tile';
-import { MapContainer, Marker, Popup } from 'react-leaflet';
+import MapMarker from '../map-marker/map-marker';
+import { MapContainer } from 'react-leaflet';
 
 function Map() {
   return (
-    <MapContainer center={[40.65, -73.91]} zoom={13} scrollWheelZoom={false} id="mapid">
+    <MapContainer center={[40.65, -73.91]} zoom={13} scrollWheelZoom={true} id="mapid">
       <AttributionTile />
-      <Marker position={[40.65, -73.91]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <MapMarker lat={40.65} long={-73.91} />
     </MapContainer>
   );
 }
