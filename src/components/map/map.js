d0f4265edc2c535f25d18f4ecc719f110513dccd
Jonathan Chin <jonathankchin@gmail.com>
2021-04-17 22:22:41 -0400

    feat: add find average geo to find center of map

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import AttributionTile from './../attribution-tile/attribution-tile';
 import MapMarker from '../map-marker/map-marker';
 import api from './../../api/index.js';
+import findAverageGeo from './../../api/utils/findAverageGeo';
 import { MapContainer } from 'react-leaflet';
 
 const createMapMarkers = (data) => {
@@ -10,24 +11,37 @@ const createMapMarkers = (data) => {
     return <MapMarker key={item.unique_key} lat={item.latitude} long={item.longitude} />
   });
 }
+
+const getCoordinates = (dataArray) => {
+  const coordinates = [];
+  dataArray.forEach((data) => {
+    coordinates.push({latitude: data.latitude, longitude: data.longitude});
+  });
+  return coordinates;
+}
 class Map extends Component {
   constructor () {
     super()
     this.state = {
-      markerData: ''
+      markerData: '',
+      center: ''
     }
   }
   
   componentDidMount() {
     api().then((data) => { 
+      const centerCoordinates = findAverageGeo(getCoordinates(data))
       this.setState({markerData: data}) 
-    })
+      this.setState({center: [parseFloat(centerCoordinates.latitude.toFixed(2)), parseFloat(centerCoordinates.longitude.toFixed(2))]})
+    });
+
   }
 
   render() {
+    const isAppReady = this.state.markerData && this.state.center;
     return (
-      this.state.markerData ?       
-        <MapContainer center={[40.65, -73.91]} zoom={10} scrollWheelZoom={true} id="mapid">
+      isAppReady ?       
+        <MapContainer center={this.state.center} zoom={10} scrollWheelZoom={true} id="mapid">
           <AttributionTile />
           {
             createMapMarkers(this.state.markerData)
