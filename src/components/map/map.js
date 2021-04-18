611a113943eb83658748456b62701dadda424283
Jonathan Chin <jonathankchin@gmail.com>
2021-04-17 21:24:01 -0400

    feat: update map to use api call to map nyc data

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,22 +1,42 @@
 import './map.css';
+import React, {Component} from 'react';
 import AttributionTile from './../attribution-tile/attribution-tile';
 import MapMarker from '../map-marker/map-marker';
-import data from './../../api/test/mock-data.json';
+import api from './../../api/index.js';
 import { MapContainer } from 'react-leaflet';
 
-function Map() {
-  const mapData = data.map((item)=> {
-    return <MapMarker key={item.unique_key} lat={item.latitude} long={item.longitude} />}
-  )
-  return (
-    <MapContainer center={[40.65, -73.91]} zoom={13} scrollWheelZoom={true} id="mapid">
-      <AttributionTile />
-   {
-     mapData
-   }
-      <MapMarker lat={40.65} long={-73.91} />
-    </MapContainer>
-  );
+const createMapMarkers = (data) => {
+  return data.map((item)=> {
+    return <MapMarker key={item.unique_key} lat={item.latitude} long={item.longitude} />
+  });
+}
+class Map extends Component {
+  constructor () {
+    super()
+    this.state = {
+      markerData: ''
+    }
+  }
+  
+  componentDidMount() {
+    api().then((data) => { 
+      this.setState({markerData: data}) 
+    })
+  }
+
+  render() {
+    console.log(this.state.markerData)
+    return (
+      this.state.markerData ?       
+        <MapContainer center={[40.65, -73.91]} zoom={13} scrollWheelZoom={true} id="mapid">
+          <AttributionTile />
+          {
+            createMapMarkers(this.state.markerData)
+          }
+      </MapContainer> : 
+      <h1>Loading...</h1>
+    );
+  }
 }
 
 export default Map;
