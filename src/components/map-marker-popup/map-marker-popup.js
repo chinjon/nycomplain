01ea0865cbd6e63175ca96edc355bab6c3e35821
Jonathan Chin <jonathankchin@gmail.com>
2021-05-09 19:24:28 -0400

    feat: refactor label into module

--- src/components/map-marker-popup/map-marker-popup.js
+++ src/components/map-marker-popup/map-marker-popup.js
@@ -1,15 +1,13 @@
 import './map-marker-popup.css'
 import React, {Component} from 'react';
 import { Popup } from 'react-leaflet';
-
-const createComplaintLabel = (complaintType = null) => <p><span class="popup-label-bold">Complaint Type:</span> <span class="popup-complaint-text-italic">{complaintType}</span></p>
-const createStatusLabel = (statusLabel = null) => <p><span class="popup-label-bold">Status:</span> <span class="popup-complaint-text-italic">{statusLabel}</span></p>
+import createMarkerPopupLabel from './utils/create-marker-popup-label.js';
 class MapMarkerPopup extends Component {
   render() {
     return (
       <Popup>
-        {createComplaintLabel(this.props.complaintType)}
-        {createStatusLabel(this.props.status)}
+        {createMarkerPopupLabel('Complaint Type:', this.props.complaintType)}
+        {createMarkerPopupLabel('Status:', this.props.status)}
       </Popup>
     );
   }
---
+++ src/components/map-marker-popup/utils/create-marker-popup-label.js
@@ -0,0 +1,3 @@
+const createMarkerPopupLabel = (label, content) => <p><span class="popup-label-bold">{label}</span> <span class="popup-content-text-italic">{content}</span></p>
+
+export default createMarkerPopupLabel;
