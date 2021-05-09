8795a6ea4f3a96d7f18f9b94a9e874e0040df368
Jonathan Chin <jonathankchin@gmail.com>
2021-05-09 19:31:57 -0400

    feat: init commit of component

---
+++ src/components/complaints-list/complaint-list.js
@@ -0,0 +1,25 @@
+import './complaint-list.css';
+import React, {Component} from 'react';
+
+class ComplaintList extends Component {
+  constructor () {
+    super()
+    this.state = {
+    }
+  }
+  
+  componentDidMount() {
+   
+  }
+
+  render() {
+    
+    return (
+      <div>
+        <ul></ul>
+      </div>
+    );
+  }
+}
+
+export default ComplaintList;
