a0dc651379d17f0ae417bbd30b6d6a5e3df496b4
Jonathan Chin <jonathankchin@gmail.com>
2021-05-11 10:34:03 -0400

    feat: add key to complaint list

--- src/components/complaints-list/complaint-list.js
+++ src/components/complaints-list/complaint-list.js
@@ -1,12 +1,18 @@
 import './complaint-list.css';
 import React, {Component} from 'react';
-
+import countComplaints from './../utils/count-complaints'
 class ComplaintList extends Component {
+
+  componentDidMount() {
+    console.log(countComplaints(this.props.complaints))
+
+  }
+
   render() {
     return (
       <div>
         {
-          this.props.complaints.map((complaint) => <p>{complaint.complaint_type}</p>)
+          this.props.complaints.map((complaint) => <p key={complaint.unique_key}>{complaint.complaint_type}</p>)
         }
       </div>
     );
