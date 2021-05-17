c2708c1f1ac0713b4aa2914e97411dfdee62e7d2
Jonathan Chin <jonathankchin@gmail.com>
2021-05-17 18:12:12 -0400

    feat: update complaint list with bar chart

--- src/components/complaints-list/complaint-list.js
+++ src/components/complaints-list/complaint-list.js
@@ -1,20 +1,33 @@
 import './complaint-list.css';
 import React, {Component} from 'react';
+import { Bar } from 'react-chartjs-2';
 import complaintCount from '../utils/complaint-count'
 import complaintListFilter from '../utils/complaint-list-filter'
+import createFormattedChartData from '../utils/create-formatted-chart-data'
 class ComplaintList extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+    }
+  }
 
   componentDidMount() {
     console.log(complaintCount(this.props.complaints))
     console.log(complaintListFilter(this.props.complaints))
+    const complaintCountObj = complaintCount(this.props.complaints);
+    const formattedData = createFormattedChartData(complaintCountObj);
+    console.log(createFormattedChartData(complaintCountObj))
+    this.setState({chartData: {labels: formattedData.labels, datasets: [{data: formattedData.data}]} })
   }
 
   render() {
     return (
       <div>
-        {
+        {/* {
           this.props.complaints.map((complaint) => <p key={complaint.unique_key}>{complaint.complaint_type}</p>)
-        }
+        } */}
+        <Bar data={this.state.chartData}></Bar>
       </div>
     );
   }
