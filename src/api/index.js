0f09acd561ae2a11a43660e7360980964866cb2e
Jonathan Chin <jonathankchin@gmail.com>
2021-05-21 12:00:58 -0400

    feat: break up search query into variables

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const fetch = require('node-fetch');
 const writeToTextFile = require('./utils/write-to-text-file.js')
 const BASE_API_URL = 'https://data.cityofnewyork.us/resource/erm2-nwe9.json?';
-const searchQuery = `$where=created_date between '2021-04-20T17:00:00' and '2021-04-20T17:45:00'`;
+const WHERE_CREATED_DATE = `$where=created_date`
+const BETWEEN = `between`
+const searchQuery = `${WHERE_CREATED_DATE} ${BETWEEN} '2021-04-20T17:00:00' and '2021-04-20T17:45:00'`;
 
 const api = async () => {
   const response = await fetch(`${BASE_API_URL}${searchQuery}`, {
