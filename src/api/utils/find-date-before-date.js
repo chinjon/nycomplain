496d3d1300766e95938d7e45e179edb20aaffdd4
Jonathan Chin <jonathankchin@gmail.com>
2021-05-22 18:54:53 -0400

    feat: replace function

--- src/api/utils/find-date-before-date.js
+++
@@ -1,7 +0,0 @@
-const daysFn = require('date-fns')
-
-const findDateBeforeDate = (startDate = new Date(), daysBefore = 1) => {
-  return daysFn.subDays(startDate, daysBefore);
-}
-
-module.exports = findDateBeforeDate;
