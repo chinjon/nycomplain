df644d0afa440d2a59f3d0680227b4707746f738
Jonathan Chin <jonathankchin@gmail.com>
2021-05-28 17:19:35 -0400

    feat: add status viz

--- src/components/complaints-viz/complaint-viz.js
+++ src/components/complaints-viz/complaint-viz.js
@@ -21,13 +21,19 @@ class ComplaintViz extends Component {
     const boroughCountObj = countPropsByKey(this.props.complaints, 'borough');
     const boroughFormattedData = createFormattedChartData(boroughCountObj);
     const boroughColors = generateVizColors(boroughFormattedData.data);
-    this.setState({boroughData: {labels: boroughFormattedData.labels, datasets: [{label: 'by Borough', data: boroughFormattedData.data, backgroundColor: boroughColors}]}})
+    this.setState({boroughData: {labels: boroughFormattedData.labels, datasets: [{label: 'by Borough', data: boroughFormattedData.data, backgroundColor: boroughColors}]}});
+
+    const statusCountObj = countPropsByKey(this.props.complaints, 'status');
+    const statusFormattedData = createFormattedChartData(statusCountObj)
+    const statusColors = generateVizColors(statusFormattedData.data);
+    this.setState({statusData: {labels: statusFormattedData.labels, datasets: [{label: 'by Status', data: statusFormattedData.data, backgroundColor: statusColors}]}})
   }
 
   render() {
     return (
       <div className="complaint-viz">
         <Bar data={this.state.complaintData}></Bar>
+        <Bar data={this.state.statusData}></Bar>
         <PolarArea data={this.state.boroughData}></PolarArea>
       </div>
     );
