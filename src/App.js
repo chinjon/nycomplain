c7fbc7d22e8324f983fd72f25b049d0765d6ef3e
Jonathan Chin <jonathankchin@gmail.com>
2021-05-30 19:39:42 -0400

    feat: move sorting of data for charts

--- src/App.js
+++ src/App.js
@@ -5,24 +5,45 @@ import Search from './components/search/search'
 import ComplaintViz from './components/complaints-viz/complaint-viz.js';
 import api from './api/index';
 import createSearchQuery from './components/utils/create-search-query.js'
-
+import countPropsByKey from './components/utils/count-props-by-key'
+import createFormattedChartData from './components/utils/create-formatted-chart-data'
+import generateVizColors from './components/complaints-viz/utils/generate-viz-colors'
 class App extends Component {
   constructor () {
     super()
     this.state = {
       data: null,
-      date: '2021-05-20'
+      date: '2021-05-20',
+      complaintData: null,
+      statusData: null,
+      boroughData: null,
     }
   }
 
   componentDidMount() {
     this.callApi(this.state.date);
   }
-
+  
   callApi = (date) => {
     api(createSearchQuery(date, '17:00:00', '17:45:00')).then((data) => { 
       this.setState({data: data}); 
+      this.setState({
+        complaintData: this.createVisualData(this.state.data, 'complaint_type'),
+        statusData: this.createVisualData(this.state.data, 'status'),
+        boroughData: this.createVisualData(this.state.data, 'borough')
+      })
     });
+  }
+
+  createVisualData = (data, keyFilter) => {
+    const dataObject = countPropsByKey(data, keyFilter);
+    const dataset = createFormattedChartData(dataObject);
+    const colors = generateVizColors(dataset.data);
+
+    return {
+      dataset,
+      colors
+    }
   }
   
   getDate = (date) => {
@@ -30,7 +51,7 @@ class App extends Component {
     this.callApi(this.state.date);
   }  
   render() {
-    const isAppReady = this.state.data !== null;
+    const isAppReady = this.state.data !== null && this.state.complaintData !== null && this.state.statusData !== null && this.state.boroughData !== null;
 
     return (
       isAppReady ? 
@@ -43,7 +64,7 @@ class App extends Component {
             <Map data={this.state.data} />
           </div>
           <div className="complaint-viz">
-            <ComplaintViz complaints={this.state.data}></ComplaintViz>
+            <ComplaintViz statusData={this.state.statusData} boroughData={this.state.boroughData} complaintData={this.state.complaintData}></ComplaintViz>
           </div>
         </section>
       </div>
--- src/components/complaints-viz/complaint-viz.js
+++ src/components/complaints-viz/complaint-viz.js
@@ -1,40 +1,25 @@
 import './complaint-viz.css';
 import React, {Component} from 'react';
 import { Bar, PolarArea } from 'react-chartjs-2';
-import countPropsByKey from '../utils/count-props-by-key'
-import createFormattedChartData from '../utils/create-formatted-chart-data'
-import generateVizColors from './utils/generate-viz-colors';
+
 class ComplaintViz extends Component {
-
-  constructor () {
-    super()
-    this.state = {
+  setVizDataObj = (data, legendText) => {
+    return {
+      labels: data.dataset.labels, 
+      datasets: [{
+        label: legendText,
+        data: data.dataset.data,
+        backgroundColor: data.colors
+      }]
     }
-  }
-
-  componentDidMount() {
-    const complaintCountObj = countPropsByKey(this.props.complaints, 'complaint_type');
-    const complaintFormattedData = createFormattedChartData(complaintCountObj);
-    const complaintColors = generateVizColors(complaintFormattedData.data);
-    this.setState({complaintData: {labels: complaintFormattedData.labels, datasets: [{label: '# of Complaints', data: complaintFormattedData.data, backgroundColor: complaintColors}]} })
-
-    const boroughCountObj = countPropsByKey(this.props.complaints, 'borough');
-    const boroughFormattedData = createFormattedChartData(boroughCountObj);
-    const boroughColors = generateVizColors(boroughFormattedData.data);
-    this.setState({boroughData: {labels: boroughFormattedData.labels, datasets: [{label: 'by Borough', data: boroughFormattedData.data, backgroundColor: boroughColors}]}});
-
-    const statusCountObj = countPropsByKey(this.props.complaints, 'status');
-    const statusFormattedData = createFormattedChartData(statusCountObj)
-    const statusColors = generateVizColors(statusFormattedData.data);
-    this.setState({statusData: {labels: statusFormattedData.labels, datasets: [{label: 'by Status', data: statusFormattedData.data, backgroundColor: statusColors}]}})
   }
 
   render() {
     return (
       <div className="complaint-viz">
-        <Bar data={this.state.complaintData}></Bar>
-        <Bar data={this.state.statusData}></Bar>
-        <PolarArea data={this.state.boroughData}></PolarArea>
+        <Bar data={this.setVizDataObj(this.props.complaintData, '# of complaints')}></Bar>
+        <Bar data={this.setVizDataObj(this.props.statusData, 'by status')}></Bar>
+        <PolarArea data={this.setVizDataObj(this.props.boroughData, 'by borough')}></PolarArea>
       </div>
     );
   }
