282ec5e3a6abb0bbaaa5f7885850a72a99763f4d
Jonathan Chin <jonathankchin@gmail.com>
2021-06-06 11:20:33 -0400

    feat: update alpha value

--- src/components/complaints-viz/utils/generate-random-rbg.js
+++ src/components/complaints-viz/utils/generate-random-rbg.js
@@ -2,7 +2,7 @@ const generateRandomRgba = () => {
   const o = Math.round ;
   const r = Math.random ;
   const s = 255;
-  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 1 + ')';
 }
 
 export default generateRandomRgba;
