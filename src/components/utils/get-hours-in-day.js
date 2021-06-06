45cc06657705299064e24d28eccf69c0557f6712
Jonathan Chin <jonathankchin@gmail.com>
2021-06-06 19:48:57 -0400

    feat: update formatting of time

--- src/components/utils/get-hours-in-day.js
+++ src/components/utils/get-hours-in-day.js
@@ -1,12 +1,18 @@
 const eachHourOfInterval = require('date-fns').eachHourOfInterval;
+const format = require('date-fns').format;
 
 const getHoursInDay = () => {
-  return eachHourOfInterval({
+  const datesObj = eachHourOfInterval({
     start: new Date(2021, 1, 1, 0),
     end: new Date(2021, 1, 1, 23)
+  });
+  const datesFormatted = [];
+
+  datesObj.forEach((date) => {
+    datesFormatted.push(format(date, 'hh:mm:ss'))
   })
+
+  return datesFormatted;
 }
 
-
-console.log(getHoursInDay())
-// export default getHoursInDay;
+export default getHoursInDay;
