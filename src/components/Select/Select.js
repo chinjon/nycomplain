a7efba058bc8b74e65dff4a2fc54be1ed168f0a7
Jonathan Chin <jonathankchin@gmail.com>
2021-06-27 19:11:37 -0400

    feat: update props for start and end time

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
 class Select extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      value: ''
+    }
+  }
  
   render() {
     return (
-      <select name={this.props.name} id={this.props.selectId}>
+      <select name={this.props.name} id={this.props.selectId} value={this.state.value} onChange={this.props.handleOnChange}>
         {(this.props.selectOptions).map((option) => {
           return <option value={option} key={`${this.props.name}-${option}`}>{option}</option>
         })}
