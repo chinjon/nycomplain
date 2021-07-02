25f3644e46cb0b57e2dd12e0cd5b501077bb8503
Jonathan Chin <jonathankchin@gmail.com>
2021-07-02 18:21:22 -0400

    feat: update handle on change function

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,10 +8,20 @@ class Select extends Component {
       value: ''
     }
   }
+
+  componentDidMount() {
+    if (this.props.value) {
+      this.setState({value: this.props.value})
+    }
+  }
+
+  handleOnChange = (event) =>  {
+    this.setState({value: event.target.value})
+  }
  
   render() {
     return (
-      <select name={this.props.name} id={this.props.selectId} value={this.state.value} onChange={this.props.handleOnChange}>
+      <select name={this.props.name} id={this.props.selectId} value={this.state.value} onChange={this.handleOnChange}>
         {(this.props.selectOptions).map((option) => {
           return <option value={option} key={`${this.props.name}-${option}`}>{option}</option>
         })}
